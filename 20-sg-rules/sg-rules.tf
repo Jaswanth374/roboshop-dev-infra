54ce83430f800dec3c084488f072d0d90ef10cc7
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-03-13 07:42:37 +0530

    terraform commit

--- 20-sg-rules/sg-rules.tf
+++ 20-sg-rules/sg-rules.tf
@@ -79,12 +79,20 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   source_security_group_id = local.bastion_sg_id
 }
 
-resource "aws_security_group_rule" "backend_alb_bastion" {
+resource "aws_security_group_rule" "catalogue_bastion" {
   type              = "ingress"
-  from_port         = 80
-  to_port           = 80
+  from_port         = 22
+  to_port           = 22
   protocol          = "tcp"
-  security_group_id = local.backend_alb_sg_id
+  security_group_id = local.catalogue_sg_id
   source_security_group_id = local.bastion_sg_id
 }
 
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  security_group_id = local.catalogue_sg_id
+  source_security_group_id = local.backend_alb_sg_id
+}
