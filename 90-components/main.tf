fdb2e2d8d3bfbb3a00611d9a039e94e878533a78
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-03-20 09:46:06 +0530

    terraform commit

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -2,5 +2,4 @@ module "main" {
     source = "git::https://github.com/Jaswanth374/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
-
 }
